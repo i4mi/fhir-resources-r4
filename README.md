e1c38928429bbdaff674ef24771b3026bec6d063
ies1 <ies1@bfh.ch>
2019-02-08 18:26:46 +0100

    initial commit --> resources are comming tonight

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fhir-resources.ts
 FHIR resources definitions
+
+IN DEVELOPMENT -- DO NOT USE ATM
+
+
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+//https://hackernoon.com/import-json-into-typescript-8d465beded79
+//https://webpack.js.org/guides/typescript/
+//https://itnext.io/step-by-step-building-and-publishing-an-npm-typescript-package-44fe7164964c
+
+
+//SETUP TESTS WITH JEST!

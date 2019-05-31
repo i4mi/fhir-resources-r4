07886ea9c2a51476e6cab20f7045aa55c31be89d
Iseli Stefan <stefan.iseli@bfh.ch>
2019-05-31 05:25:22 +0200

    delete old stuff #8 and remove unused code.

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,5 @@
-//https://hackernoon.com/import-json-into-typescript-8d465beded79
-//https://webpack.js.org/guides/typescript/
-//https://itnext.io/step-by-step-building-and-publishing-an-npm-typescript-package-44fe7164964c
-
 /**
  * EXPORTS OF I4MI Resources
  */
 export { I4MIBundle, I4MIInterfaceToMapResource } from './i4mi_resources/v4.0.0/bundle';
 export * from './apiMethods/apiMethods';
-/**
- * This function exports you the library for the given fhir version
- * The supported fhir versions are given in the SupportedFhirVersion enum type
- * If no version is given, it will always take the latest
- * @param version Version for fhir resources to export
- *//*
-export function getResources(version?: SupportedFhirVersions) {
-    if (typeof version === 'undefined') {
-        console.log(`No version given. Exports latest (${SupportedFhirVersions.latest}) supported fhir version.`);
-        // let importFunction = function() {
-        //     import('./definition/version4.0.0')
-        // }
-        return import('./definition/v4.0.0');
-    }
-
-    switch (version) {
-        case SupportedFhirVersions.V4_0_0:
-            console.log('dynamic export R4');
-            //return import('./definition/version4.0.0/');
-        case SupportedFhirVersions.V3_0_1:
-            console.log('dynamic export STU3');
-            return 'NOT_IMPLEMENTED_YET';
-    }
-}
-
-export enum SupportedFhirVersions {
-    latest = 'V4_0_0',
-    V4_0_0 = 'V4_0_0',
-    V3_0_1 = 'V3_0_1'
-}*/

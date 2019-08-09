a5f6d4b4bb4781eb5ab0b2a2c7f160948662d691
Iseli Stefan <stefan.iseli@bfh.ch>
2019-08-09 18:53:48 +0200

    upload dist

---
+++ dist/definition/index.js
@@ -0,0 +1,6 @@
+"use strict";
+/**
+ * Created by Insitut for Medical Informatics (I4MI) - Department of Engineering and Information Technology - Bern University of Applied Science (BFH)
+ * https://www.i4mi.ti.bfh.ch
+ */
+module.exports = fhir;
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+var bundle_1 = require("./smartResources/bundle");
+exports.I4MIBundle = bundle_1.I4MIBundle;
+__export(require("./apiMethods/apiMethods"));
